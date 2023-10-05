00624f4b3741a542661aa4f965d237a85a4b7c2f
dhoiffahmad <dhoiff.ahmad@gmail.com>
2023-10-05 12:30:00 +0700

    transisi nav cek

--- js/js.js
+++ js/js.js
@@ -6,7 +6,7 @@ const menu_items = document.querySelectorAll('nav .mainMenu li a');
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
 
-// close menu when you click on a menu item 
+
 menu_items.forEach(item => {
     item.addEventListener('click',function(){
         close();
@@ -20,31 +20,3 @@ function show(){
 function close(){
     mainMenu.style.top = '-100%';
 }
-
-
-// Scroll Smoth
-
-// $(document).ready(function(){
-//     // Add smooth scrolling to all links
-//     $("a").on('click', function(event) {
-  
-//       // Make sure this.hash has a value before overriding default behavior
-//       if (this.hash !== "") {
-//         // Prevent default anchor click behavior
-//         event.preventDefault();
-  
-//         // Store hash
-//         var hash = this.hash;
-  
-//         // Using jQuery's animate() method to add smooth page scroll
-//         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//         $('html, body').animate({
-//           scrollTop: $(hash).offset().top
-//         }, 800, function(){
-  
-//           // Add hash (#) to URL when done scrolling (default click behavior)
-//           window.location.hash = hash;
-//         });
-//       } // End if
-//     });
-//   });
